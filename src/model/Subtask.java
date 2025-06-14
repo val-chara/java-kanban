8cceb2a3d35af81b248f6d6e6117fa98d46c704b
valchara <charyevalery@mail.ru>
2025-06-14 23:13:25 +0300

    Исправила стилистику 3

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,21 +1,27 @@
 package model;
 
 public class Subtask extends Task {
+
     private int epicId;
 
     public Subtask(String title, String description, Status status, int epicId) {
+
         super(title, description, status);
         this.epicId = epicId;
     }
+
     public int getEpicId() {
         return epicId;
     }
+
     public void setEpicId(int epicId) {
         this.epicId = epicId;
     }
 
     @Override
+
     public String toString() {
+
         return "Subtask{" +
                 "id=" + getId() +
                 ", name='" + getTitle() + '\'' +
@@ -24,5 +30,4 @@ public class Subtask extends Task {
                 ", epicId=" + getEpicId() +
                 '}';
     }
-
 }
