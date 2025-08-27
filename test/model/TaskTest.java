0dadcf0a0223e7d0c52581311f000f94bd8a8f18
valchara <charyevalery@mail.ru>
2025-08-27 14:24:16 +0300

    Исправляю логику

--- test/model/TaskTest.java
+++ test/model/TaskTest.java
@@ -10,10 +10,15 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 class TaskTest {
+
     @Test
     void tasksWithSameIdShouldBeEqual() {
-        Task task1 = new Task("Задача 1", "Описание первой задачи", Status.NEW, LocalDateTime.of(2025, 7, 18, 10, 0), Duration.ofMinutes(90));
-        Task task2 = new Task("Задача 1", "Описание первой задачи", Status.NEW, LocalDateTime.of(2025, 7, 19, 11, 0), Duration.ofMinutes(90));
+
+        LocalDateTime sameTime = LocalDateTime.of(2025, 7, 18, 10, 0);
+        Duration sameDuration = Duration.ofMinutes(90);
+
+        Task task1 = new Task("Задача 1", "Описание первой задачи", Status.NEW, sameTime, sameDuration);
+        Task task2 = new Task("Задача 1", "Описание первой задачи", Status.NEW, sameTime, sameDuration);
         task1.setId(1);
         task2.setId(1);
 
