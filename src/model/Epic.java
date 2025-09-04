d9d7ac53296b888f3dc2608b161d1f19246e3e51
valchara <charyevalery@mail.ru>
2025-09-04 18:49:32 +0300

    Исправляю стиль

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Optional;
-
 
 public class Epic extends Task {
 
