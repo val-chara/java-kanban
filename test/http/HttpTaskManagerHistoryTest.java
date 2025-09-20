bacad0689a5b5513eefbcea62a13050854883ceb
valchara <charyevalery@mail.ru>
2025-09-20 22:07:10 +0300

    Добавила тестовый класс HttpTaskManagerHistoryTest

--- test/http/HttpTaskManagerHistoryTest.java
+++ test/http/HttpTaskManagerHistoryTest.java
@@ -1,4 +1,78 @@
 package http;
 
-public class HttpTaskManagerHistoryTest {
+import com.google.gson.Gson;
+import manager.InMemoryTaskManager;
+import manager.TaskManager;
+import model.Task;
+import model.Status;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpTaskManagerHistoryTest {
+    private TaskManager manager;
+    private HttpTaskServer taskServer;
+    private Gson gson;
+    private HttpClient client;
+
+    public HttpTaskManagerHistoryTest() throws IOException {
+        manager = new InMemoryTaskManager();
+        taskServer = new HttpTaskServer(manager);
+        gson = BaseHttpHandler.getGson();
+        client = HttpClient.newHttpClient();
+    }
+
+    @BeforeEach
+    void setUp() {
+        manager.deleteAllTasks();
+        manager.deleteAllSubtasks();
+        manager.deleteAllEpics();
+        taskServer.start();
+    }
+
+    @AfterEach
+    void shutDown() {
+        taskServer.stop();
+    }
+
+    @Test
+    void testGetHistory() throws IOException, InterruptedException {
+        Task task = new Task("Test task", "Testing task", Status.NEW,
+                LocalDateTime.now(), Duration.ofMinutes(30));
+        Task createdTask = manager.createTask(task);
+
+        manager.getTaskById(createdTask.getId());
+
+        URI url = URI.create("http://localhost:8080/history");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+
+        Task[] history = gson.fromJson(response.body(), Task[].class);
+        assertEquals(1, history.length);
+        assertEquals("Test task", history[0].getTitle());
+    }
+
+    @Test
+    void testGetEmptyHistory() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/history");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+
+        Task[] history = gson.fromJson(response.body(), Task[].class);
+        assertEquals(0, history.length);
+    }
 }
