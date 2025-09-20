2adc2a0e8bc2c30936e22a4ef2fa91c7b2323625
valchara <charyevalery@mail.ru>
2025-09-20 22:04:18 +0300

    Добавила тестовый класс HttpTaskManagerSubtasksTest с проверкой эндпоинтов

--- test/http/HttpTaskManagerSubtasksTest.java
+++ test/http/HttpTaskManagerSubtasksTest.java
@@ -1,4 +1,100 @@
 package http;
 
-public class HttpTaskManagerSubtasksTest {
+import com.google.gson.Gson;
+import manager.InMemoryTaskManager;
+import manager.TaskManager;
+import model.Epic;
+import model.Status;
+import model.Subtask;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpTaskManagerSubtasksTest {
+    private TaskManager manager;
+    private HttpTaskServer taskServer;
+    private Gson gson;
+    private HttpClient client;
+    private Epic epic;
+
+    public HttpTaskManagerSubtasksTest() throws IOException {
+        manager = new InMemoryTaskManager();
+        taskServer = new HttpTaskServer(manager);
+        gson = BaseHttpHandler.getGson();
+        client = HttpClient.newHttpClient();
+    }
+
+    @BeforeEach
+    void setUp() {
+        manager.deleteAllTasks();
+        manager.deleteAllSubtasks();
+        manager.deleteAllEpics();
+
+        epic = new Epic("Test epic", "Testing epic", Status.NEW);
+        manager.createEpic(epic);
+
+        taskServer.start();
+    }
+
+    @AfterEach
+    void shutDown() {
+        taskServer.stop();
+    }
+
+    @Test
+    void testAddSubtask() throws IOException, InterruptedException {
+        Subtask subtask = new Subtask("Test subtask", "Testing subtask", Status.NEW,
+                epic.getId(), LocalDateTime.now(), Duration.ofMinutes(30));
+        String subtaskJson = gson.toJson(subtask);
+
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .POST(HttpRequest.BodyPublishers.ofString(subtaskJson))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(201, response.statusCode());
+
+        List<Subtask> subtasksFromManager = manager.getAllSubtasks();
+        assertEquals(1, subtasksFromManager.size());
+        assertEquals("Test subtask", subtasksFromManager.get(0).getTitle());
+    }
+
+    @Test
+    void testGetSubtasks() throws IOException, InterruptedException {
+        Subtask subtask = new Subtask("Test subtask", "Testing subtask", Status.NEW,
+                epic.getId(), LocalDateTime.now(), Duration.ofMinutes(30));
+        manager.createSubtask(subtask);
+
+        URI url = URI.create("http://localhost:8080/subtasks");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+
+        Subtask[] subtasks = gson.fromJson(response.body(), Subtask[].class);
+        assertEquals(1, subtasks.length);
+        assertEquals("Test subtask", subtasks[0].getTitle());
+    }
+
+    @Test
+    void testGetSubtaskByIdNotFound() throws IOException, InterruptedException {
+        URI url = URI.create("http://localhost:8080/subtasks/999");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(404, response.statusCode());
+    }
 }
